86064b6f2675afcc2611aacc94f1c4da594bf946
MatheusBecali <matheus_becali@hotmail.com>
2026-08-17 14:58:28 -0300

    fix: change aggregate_results bash to python

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 bash ./run.sh
-bash ./run_aggregate_results.sh
+python aggregate_results.py
 bash ./run_latex.sh
 python generate_decision_making_csv.py --output-dir ./results/decision_making
 python atopsis_analysis.py 
